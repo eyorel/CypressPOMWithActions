d7ceac539cbc52f175904836045d7a31352cdd9b
E Tester <tester@ninjaclouder.com>
2024-07-03 00:09:03 -0700

    Improving Reporting tool

--- cypress.config.js
+++ cypress.config.js
@@ -1,16 +1,47 @@
-const { defineConfig } = require("cypress");
+const { defineConfig } = require('cypress')
+const { beforeRunHook, afterRunHook } = require('cypress-mochawesome-reporter/lib');
+const fs = require('fs')
 
 module.exports = defineConfig({
+  viewportWidth: 1920,
+  viewportHeight: 1080,
+  defaultCommandTimeout: 50000,
+  reporter: 'cypress-mochawesome-reporter',
+  reporterOptions: {
+    reportDir: 'cypress/reports',
+    charts: true,
+    reportPageTitle: 'Test Report',
+    embeddedScreenshots: true,
+    inlineAssets: true,
+    videoOnFailOnly: true,
+  },
+  video: true,
+  screenshotsFolder: 'cypress/screenshots',
+  videosFolder: 'cypress/videos',
+  trashAssetsBeforeRuns: true,
   e2e: {
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      on('after:spec', (spec, results) => {
+        if (results && results.video) {
+          // Do we have failures for any retry attempts?
+          const failures = results.tests.some((test) =>
+            test.attempts.some((attempt) => attempt.state === 'failed')
+          )
+          if (!failures) {
+            // delete the video if the spec passed and no tests retried
+            fs.unlinkSync(results.video)
+          }
+        }
+      })
+      on('before:run', async (details) => {
+        console.log('override before:run');
+        await beforeRunHook(details);
+      });
+
+      on('after:run', async (results) => {
+        console.log('override after:run');
+        await afterRunHook();
+      });
     },
-    "reporter": "mochawesome",
-    "reporterOptions": {
-      "reportDir": "cypress/reports",
-      "overwrite": false,
-      "html": true,
-      "json": true
-    }
   },
 });
